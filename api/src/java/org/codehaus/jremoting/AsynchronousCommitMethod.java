afdf2b6fee63fed64df03ccdcd60d2cd63ab4d77
mauro <mauro@ebf96e7e-f801-0410-8dc7-a7e6b35a8818>
2005-10-04 22:36:31 +0000

    Added annotations
    git-svn-id: http://svn.codehaus.org/jremoting/jremoting/trunk@27 ebf96e7e-f801-0410-8dc7-a7e6b35a8818

---
+++ api/src/java/org/codehaus/jremoting/AsynchronousCommitMethod.java
@@ -0,0 +1,18 @@
+package org.codehaus.jremoting;
+/*****************************************************************************
+ * Copyright (C) JRemoting Committers. All rights reserved.                  *
+ * ------------------------------------------------------------------------- *
+ * The software in this class is published under the terms of the BSD        *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE-BSD.txt file.                                                 *
+ *****************************************************************************/
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Retention;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AsynchronousCommitMethod {
+}
