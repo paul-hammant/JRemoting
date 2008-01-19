948b353c19fbf7264feec9370af2dd2bc7bad6c1
paul <paul@ebf96e7e-f801-0410-8dc7-a7e6b35a8818>
2008-01-19 04:43:40 +0000

    not needed
    git-svn-id: http://svn.codehaus.org/jremoting/jremoting/trunk@293 ebf96e7e-f801-0410-8dc7-a7e6b35a8818

--- api/src/java/org/codehaus/jremoting/server/ServiceHandlerAccessor.java
+++ api/src/java/org/codehaus/jremoting/server/ServiceHandlerAccessor.java
@@ -7,16 +7,6 @@ import org.codehaus.jremoting.requests.InvokeMethod;
 public interface ServiceHandlerAccessor extends Publisher {
 
     /**
-     * Get the ServiceHandler for this transport.  Used in special adapters.
-     *
-     * @param invokeMethod used as a hint for getting the right handler.
-     * @param objectName    the object name relating to the method request.
-     * @return a suitable ServiceHandler
-     */
-    ServiceHandler getServiceHandler(InvokeMethod invokeMethod, String objectName);
-
-
-    /**
      * Get the ServiceHandler for a published lookup name. Used in special adapters.
      *
      * @param service the published lookup name.
